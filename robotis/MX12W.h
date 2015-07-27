0b6f148601382d4ea2fe9e5be460ee6ed5408f6e
geox <arturwaz11@gmail.com>
2015-07-27 08:26:42 +0200

    added MX-12W servo

---
+++ robotis/MX12W.h
@@ -0,0 +1,20 @@
+//
+// Created by geox on 27.07.15.
+//
+
+
+#ifndef DYNAMIXEL_MX12W_H
+#define DYNAMIXEL_MX12W_H
+
+
+#include "MX28.h"
+
+
+
+class MX12W: public MX28 {
+
+};
+
+
+
+#endif //DYNAMIXEL_MX12W_H
